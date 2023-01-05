ba178320c7d407b1d4ee2229e27b991938e1e132
adamubida <mailadamu@gmail.com>
2023-01-05 11:49:35 -0800

    0x04python-more_data_structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return 0
-    return sum([x*y for (x, y) in my_list]) / sum([y for (x, y) in my_list])
 
-print(weight_average([4532,45]))
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
